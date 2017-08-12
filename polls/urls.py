d79238e258cba10e713c4faf5540cedc25f617a9
Daniel Montgomery <d.montgrmry@gmail.com>
2017-08-12 16:39:27 -0700

    Make index and detail views useful

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'), # /polls/
-    url(r'^(?P<question_id>[0-9]+)$', views.detail, name='detail'), # /polls/6/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'), # /polls/6/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'), # /polls/6/results/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'), # /polls/6/vote/
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 def index(request):
-    return HttpResponse('Hello, world. You\'re at the polls index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, 'polls/index.html', {'latest_questions': latest_question_list})
 
 def detail(request, question_id):
-    return HttpResponse('You\'re looking at question {0}.'.format(question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = 'You\'re looking at the results of question {0}.'
